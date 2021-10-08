8a57181a456215d945b8ca5d62fe885ee36936e3
flursky <kh41r1.abidi@gmail.com>
2021-10-08 16:53:08 +0100

    define model

---
+++ models/models.py
@@ -0,0 +1,26 @@
+from torch import nn
+import torch.nn.functional as F
+from dgl.nn.pytorch import GraphConv
+
+class GCN(nn.Module):
+    def __init__(self, feature_size, hidden_size, num_classes, dropout_rate = 0.5):
+        super(GCN, self).__init__()
+
+        
+        self.gcn = nn.Sequential(
+            GraphConv(in_feats=feature_size, out_feats=hidden_size, activation=nn.ReLU()),
+            nn.Dropout(dropout_rate),
+            GraphConv(in_feats=hidden_size, out_feats=num_classes, activation=nn.ReLU()),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, graph, n_feats):
+        x = n_feats
+
+        for layer in self.gcn:
+            if isinstance(layer, GraphConv):
+                x = layer(graph, x)
+            else:
+                x = layer(x)
+        
+        return x
